aa04007548e5f655717483c4ece0e86ce2b2e325
m_eve_admin <smjworkon@163.com>
2021-03-20 12:14:24 +0800

    调整代码结构

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -16,17 +16,22 @@ export default {
 		};
 	},
 	onLoad() {
-		// h5 和 安卓 都使用，使用h5访问的时候记得跨域
+		// h5，使用h5访问的时候记得跨域
+		// #ifdef H5
 		this.url = `${this.viewerUrl}?file=${encodeURIComponent(this.fileUrl)}`;
-		// 在安卓和ios手机上，取消下面的注释即可
+		// #endif
+		
+		// 在安卓和ios手机上
 		// 判断是手机系统：安卓，使用pdf.js
-		// if(plus.os.name === 'Android') {
-		// 	this.url = `${this.viewerUrl}?file=${encodeURIComponent(this.fileUrl)}`;
-		// }
-		// // ios，直接访问pdf所在路径
-		// else {
-		// 	this.url = encodeURIComponent(this.fileUrl);
-		// }
+		// #ifdef APP-PLUS
+		if(plus.os.name === 'Android') {
+			this.url = `${this.viewerUrl}?file=${encodeURIComponent(this.fileUrl)}`;
+		}
+		// ios，直接访问pdf所在路径
+		else {
+			this.url = encodeURIComponent(this.fileUrl);
+		}
+		// #endif
 	},
 	methods: {}
 };
